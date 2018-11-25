808404f5c9cc4a293a835689b3daf6f41de1c628
pratikjain04 <pratikjain0411@gmail.com>
2018-11-26 02:06:18 +0530

    Final App Added

--- gps/lib/main.dart
+++ gps/lib/main.dart
@@ -7,7 +7,8 @@ const API_KEY = "AIzaSyAuQc6Fh5BKC8LvEpWvb6rQM75RK1eAV44";
 void main() {
   MapView.setApiKey(API_KEY);
   runApp(new MaterialApp(
-      home: Home()
+      home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 class Home extends StatefulWidget {
@@ -38,16 +39,13 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bus 26 Tracker', style: TextStyle(color: Colors.white),),
-        centerTitle: true,
-        actions: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Search'
-            ),
+        leading: Icon(Icons.search, color: Colors.white,),
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: 'Search',
           ),
-          Icon(Icons.search, color: Colors.white,),
-        ],
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: Container(
         child: Center(
