bfb467760edffe469cc58a9b1c2b12c4f12c6f6a
pratikjain04 <pratikjain0411@gmail.com>
2018-11-26 01:34:55 +0530

    App Added
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

---
+++ gps/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:map_view/map_view.dart';
+
+const API_KEY = "AIzaSyAuQc6Fh5BKC8LvEpWvb6rQM75RK1eAV44";
+
+
+void main() {
+  MapView.setApiKey(API_KEY);
+  runApp(new MaterialApp(
+      home: Home()
+  ));
+}
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  MapView mapView = MapView();
+
+  Marker marker = new Marker('1', 'Bus', 26.84264648, 75.56468724, color: Colors.red);
+
+  void showMap() {
+    mapView.onMapReady.listen((_){
+      mapView.setMarkers([marker]);
+    });
+    mapView.show(new MapOptions(
+      mapViewType: MapViewType.normal,
+      showUserLocation: true,
+      initialCameraPosition: new CameraPosition(
+          new Location(26.84264648, 75.56468724), 16.0),
+      title: "Current Bus Location",
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bus 26 Tracker', style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        actions: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Search'
+            ),
+          ),
+          Icon(Icons.search, color: Colors.white,),
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            color: Colors.blue,
+            onPressed: (){
+              showMap();
+            },
+            child: Text('BUS Location'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
